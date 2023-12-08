4460b7ae1c96b89f30cf5c1d8b1047a9dda9c0d1
TheEvilMandarin <nastyag1608@gmail.com>
2023-12-09 01:03:12 +0300

    lab3: fix makefile, lab4: fix utils

--- lab4/src/utils.c
+++ lab4/src/utils.c
@@ -6,6 +6,6 @@ void GenerateArray(int *array, unsigned int array_size, unsigned int seed) {
   srand(seed);
   for (int i = 0; i < array_size; i++) {
     array[i] = rand() % 100;
-    printf("%d\n", array[i]);
+    // printf("%d\n", array[i]);
   }
 }
